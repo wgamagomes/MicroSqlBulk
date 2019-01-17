81196b253a95edb8764bad88210f703710f80b0d
wellington <wellington.gomes@hbsis.com.br>
2019-01-17 17:12:58 -0200

    [ADD] Add Column class [ADD] Add Row class

---
+++ src/MicroSqlBulk/Column/Column.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace MicroSqlBulk
+{
+    public class Column
+    {
+        public Column(string name, PropertyDescriptor propertyDescriptor)
+        {
+            Name = name;
+            PropertyDescriptor = propertyDescriptor;
+        }
+
+        public string Name { get; }
+        public PropertyDescriptor PropertyDescriptor { get; }
+    }
+}
---
+++ src/MicroSqlBulk/Row/Row.cs
@@ -0,0 +1,13 @@
+﻿
+namespace MicroSqlBulk
+{
+   public class Row
+    {
+        public Row(params object[] values)
+        {
+            Values = values;
+        }
+
+        public object[] Values { get; }
+    }
+}
