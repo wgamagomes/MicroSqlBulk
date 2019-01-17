c9b4d51377f7e010b7c71e403a12451962ca10b7
wellington <wellington.gomes@hbsis.com.br>
2019-01-17 21:10:28 -0200

    [ADD] Add BulkInsertExtension class

---
+++ src/MicroSqlBulk/BulkInsert/BulkInsertExtension.cs
@@ -0,0 +1,34 @@
+﻿using MicroSqlBulk.Helper;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MicroSqlBulk
+{
+    public static class BulkInsertExtension
+    {
+        public static void BulkInsert<TEntity>(this IDbConnection dbConnection, List<TEntity> data, bool closeConnection = true)
+        {
+            var datatable = DataTableHelper.ConvertToDatatable(data);
+
+            SqlBulkCopy bulkCopy =
+                    new SqlBulkCopy
+                    (
+                        (SqlConnection)dbConnection,
+                        SqlBulkCopyOptions.TableLock |
+                        SqlBulkCopyOptions.FireTriggers |
+                        SqlBulkCopyOptions.UseInternalTransaction,
+                        null
+                    );
+
+            bulkCopy.DestinationTableName = datatable.TableName;
+
+            dbConnection.Open();
+
+            bulkCopy.WriteToServer(datatable);
+
+            if (closeConnection)
+                dbConnection.Close();
+        }
+    }
+}
