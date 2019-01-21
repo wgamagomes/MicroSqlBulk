500717cba57ec436c9f17d313c73cf4cc21c28f2
anderson.martins <anderson.martins@NT-03312>
2019-01-21 15:35:40 -0200

    - Added isPrimaryKey argument in Column attribute class

--- src/MicroSqlBulk/Attributes/ColumnAttribute.cs
+++ src/MicroSqlBulk/Attributes/ColumnAttribute.cs
@@ -5,11 +5,13 @@ namespace MicroSqlBulk.Attributes
     [AttributeUsage(AttributeTargets.Property)]
     public class ColumnAttribute : Attribute
     {
-        public ColumnAttribute(string name)
+        public ColumnAttribute(string name, bool isPrimaryKey = false)
         {
             Name = name;
+            IsPrimaryKey = isPrimaryKey;
         }
 
         public string Name { get; }
+        public bool IsPrimaryKey { get; set; }
     }
 }
