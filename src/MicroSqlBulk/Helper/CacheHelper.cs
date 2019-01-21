a2788e28205ecc28e897c303a225381fbfe33448
wellington <wellington.gomes@hbsis.com.br>
2019-01-21 18:36:31 -0200

    [ADD] Added ScriptTest class [ADD] Added CacheHelper class [ADD] Added GenerateLocalTempTableScript method

---
+++ src/MicroSqlBulk/Helper/CacheHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Concurrent;
+
+namespace MicroSqlBulk.Helper
+{
+    public static class CacheHelper
+    {
+        private static ConcurrentDictionary<string, SqlBulkConfiguration> _mapperCache = new ConcurrentDictionary<string, SqlBulkConfiguration>();
+
+        public static SqlBulkConfiguration GetSqlBulkEntityConfiguration<TEntity>()
+        {
+            SqlBulkConfiguration sqlBulkEntityConfiguration;
+            var nameOfT = typeof(TEntity).Name;
+
+            if (!_mapperCache.TryGetValue(nameOfT, out sqlBulkEntityConfiguration))
+            {
+                var columns = ColumnHelper.GetFildesInfo<TEntity>();
+                var tableName = TableHelper.GetTableName<TEntity>();
+                sqlBulkEntityConfiguration = new SqlBulkConfiguration(columns, tableName);
+                _mapperCache.TryAdd(nameOfT, sqlBulkEntityConfiguration);
+            }
+
+            return sqlBulkEntityConfiguration;
+        }
+
+    }
+}
--- src/MicroSqlBulk/Helper/TableHelper.cs
+++ src/MicroSqlBulk/Helper/TableHelper.cs
@@ -1,12 +1,16 @@
-﻿
-using MicroSqlBulk.Attributes;
+﻿using MicroSqlBulk.Attributes;
 using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace MicroSqlBulk.Helper
 {
     public static class TableHelper
     {
+       
+
         public static string GetTableName<TEntity>()
         {
             TableAttribute customAttribute = (TableAttribute)(typeof(TEntity).GetCustomAttributes(typeof(TableAttribute), false).FirstOrDefault());
@@ -20,5 +24,60 @@ namespace MicroSqlBulk.Helper
             return $"{schema}{tableName}";
         }
 
+        private static Dictionary<Type, String> _sqlDataMapper
+        {
+            get
+            {               
+                Dictionary<Type, String> dataMapper = new Dictionary<Type, string>();
+                dataMapper.Add(typeof(int), "BIGINT");
+                dataMapper.Add(typeof(string), "NVARCHAR(MAX)");
+                dataMapper.Add(typeof(bool), "BIT");
+                dataMapper.Add(typeof(DateTime), "DATETIME");
+                dataMapper.Add(typeof(float), "FLOAT");
+                dataMapper.Add(typeof(decimal), "DECIMAL(18,0)");
+                dataMapper.Add(typeof(Guid), "UNIQUEIDENTIFIER");
+
+                return dataMapper;
+            }
+        }
+
+        public static string GetSQLDataType(this Type type)
+        {
+            if (_sqlDataMapper.TryGetValue(type, out string dataType))
+            {
+                return dataType;
+            }
+
+            throw new KeyNotFoundException($"The element {type.Name} doesn't  match any key in the collection.");
+        }
+
+        public static string GenerateLocalTempTableScript<TEntity>()
+        {
+            var config = CacheHelper.GetSqlBulkEntityConfiguration<TEntity>();
+
+            StringBuilder script = new StringBuilder();
+
+            script.AppendLine($"CREATE TABLE #{config.TableName}_TEMP");
+            script.AppendLine("(");
+
+            for (int i = 0; i < config.Columns.Count; i++)
+            {
+                Column column = config.Columns[i];
+
+                script.Append($"\t {column.Name} {GetSQLDataType(column.PropertyDescriptor.PropertyType)}");
+
+
+                if (i != config.Columns.Count - 1)
+                {
+                    script.Append(",");
+                }
+
+                script.Append(Environment.NewLine);
+            }
+
+            script.AppendLine(")");
+
+            return script.ToString();
+        }
     }
 }
